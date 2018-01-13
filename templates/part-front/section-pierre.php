d1f66c8600cde37ac791fd8ee9fc702c7ba9af7e
aureliefb <34569019+aureliefb@users.noreply.github.com>
2018-01-14 00:25:40 +0100

    nouvelles modif : rédaction du blabla dans les fichiers section-pierre et section-savoirfaire.php : à finir + faire fonctionner les liens de navigation

--- templates/part-front/section-pierre.php
+++ templates/part-front/section-pierre.php
@@ -1,3 +1,35 @@
+<?php
+	$urlSavoirFaire = $this->generateUrl("savoirfaire");
+	$urlBoutiques   = $this->generateUrl("boutiques");
+	$urlAccueil     = $this->generateUrl("accueil");
+	$urlCatalogue   = $this->generateUrl("catalogue");
+	$urlBoutiques   = $this->generateUrl("boutiques");
+?>
+
+
 <section>
 	<h2>Section Pierre Ragot</h2>
+	<div>
+		<p>Pierre Ragot, parisien d'origine, suit un parcours d'artisan-boulanger auprès de maisons d'exception.</p>
+
+		<p>En 0000, il devient gérant du Fournil des Auffes avant de fonder sa propre marque :  la maison Saint-Honoré par Pierre Ragot. Il ouvre une première adresse dans le quartier d'Endoume en 0000, puis une seconde sur l'avenue du Prado à deux pas de la place Castellane.
+			<a href="<?php echo $urlBoutiques ?>.php">
+				<i class="fa fa-plus-circle" aria-hidden="true"></i>
+			</a>
+		</p>
+
+		<p>Sa passion du métier, son amour pour des matières premières de premier choix, son goût pour les expérimentations culinaires font la renommée de la maison Saint-Honoré. Les spécialités de la maison : le Saint-Honoré, la focaccia au charbon végétal, les bloomers, etc.
+		<a href="<?php echo $urlCatalogue ?>.php">
+				<i class="fa fa-plus-circle" aria-hidden="true"></i>
+			</a>
+		</p>
+
+		<p>Le bouche à oreilles et les articles relayés dans la presse et sur les blogs font que les marseillais se bousculent aux portes des boutiques...</p>
+
+		<p>Découvrez tout notre savoir-faire et la philosophie de notre maison ...
+			<a href="<?php echo $urlSavoirFaire ?>.php">
+				<i class="fa fa-plus-circle" aria-hidden="true"></i>
+			</a>
+		</p>
+	</div>
 </section>
--- templates/part-front/section-savoirfaire.php
+++ templates/part-front/section-savoirfaire.php
@@ -1,3 +1,27 @@
+<?php
+	$urlAccueil     = $this->generateUrl("accueil");
+	$urlCatalogue   = $this->generateUrl("catalogue");
+?>
+
 <section>
-	<h2>Section Savoir-faire</h2>
+	<h2>Notre savoir-faire</h2>
+	<section>
+		<article>
+			<div>
+				<h3>La passion du métier</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem pariatur illum aspernatur nostrum hic dolor omnis quis, dolorum perferendis obcaecati explicabo minus error sint, ducimus.</p>
+			</div>
+		</article>
+
+		<article>
+			<h3>Des matières premières de premier choix ...</h3>
+			<p><span>...pour des produits de qualité ! </span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat quos, accusamus veritatis iure facere numquam consequatur quasi ipsa perspiciatis. Nobis dicta eaque nemo harum ipsum architecto est veniam sapiente facilis blanditiis unde at, temporibus quia officiis quod cumque eius ad dolorum itaque perferendis molestiae beatae consectetur! Nostrum laboriosam dolore nihil?</p>
+			<p>Découvrez toute notre gamme de produits <a href="<?php echo $urlCatalogue ?>"><i class="fa fa-plus-circle" aria-hidden="true"></i></a></p>
+		</article>
+
+		<article>
+			<h3>On parle de nous</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni quisquam neque eaque delectus, in placeat, voluptatibus dolorem dicta consequuntur, unde qui quod voluptate ullam eveniet similique. Corporis nemo et deleniti, quas, fugiat quis reiciendis amet eveniet tempora, nesciunt facere minima!</p>
+		</article>
+	</section>
 </section>
