854b496ee80fdbd86396d5a4e5172a63ff60dbf2
aureliefb <34569019+aureliefb@users.noreply.github.com>
2018-01-13 12:39:14 +0100

    slideshow sur fil d'actus / flux rss : fonctionne

--- templates/part-front/footer.php
+++ templates/part-front/footer.php
@@ -31,8 +31,6 @@
 
 
         <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
-
-        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
 
         <script type="text/javascript" src="<?php echo $urlAccueil ?>/js/thomas/javascript.js"></script>
