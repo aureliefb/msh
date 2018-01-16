8a3f52fb1533eaf2a1d97e7b532dc1e573b8d097
ten3-1 <rukawa@ecchi.fr>
2018-01-16 11:01:42 +0100

    push

--- templates/part-back/admin-login-header.php
+++ templates/part-back/admin-login-header.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <title>Admin</title>
-        <link rel="stylesheet" href="<?php $this->generateUrl("accueil"); ?>/css/styleBack.css" />
+        <link rel="stylesheet" href="<?php echo $this->generateUrl("accueil"); ?>/css/styleBack.css" />
     </head>
     <body>
             <div class="page">
         <header>
-          <img class="logoadmin" src="<?php $this->generateUrl("accueil"); ?>/img/logolighthoriz.png">
+          <img class="logoadmin" src="<?php echo $this->generateUrl("accueil"); ?>/img/logolighthoriz.png">
         </header>
         <main>
